a1f532b334073048cd0d8790fdd4b8c94d786328
Aakash Mishra <mishra.aakash@orangemantra.in>
2025-04-28 18:05:44 +0530

    edit: compress validation rules

--- node-express-be-project/src/validations/keyRatioField.validation.mjs
+++ node-express-be-project/src/validations/keyRatioField.validation.mjs
@@ -1,153 +1,33 @@
 import Joi from 'joi';
 
-export const createKeyRatioFieldSchema = Joi.object({
-  project_id: Joi.number().required().messages({
-    'any.required': 'Project ID is required',
-    'number.base': 'Project ID must be a number'
-  }),
-  module_id: Joi.number().required().messages({
-    'any.required': 'Module ID is required',
-    'number.base': 'Module ID must be a number'
-  }),
-  building_height: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  carpet_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  saleable_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  balcony_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  built_up_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  far: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  total_external_wall_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  total_basement_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  circulation_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  service_shaft_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  window_to_area_ratio: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  drive_away_width_basement: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  total_steel_reinforcement: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  total_concrete_consumption: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  air_conditioned_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  apartment_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
+// Define a common error message
+const requiredMessage = 'Field name is required';
+const typeMessage = 'Field name must be a number';
 
-  club_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  total_parking_area: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-
-
-  total_number_of_cars: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  lobby_corridor_width: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  drive_away_width: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  floor_to_floor_height_ground_lobby: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  floor_to_floor_height_basement_b1_b2: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  floor_to_floor_height_apt: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  car_length: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  car_width: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-
-
-  passenger_lift_speed: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  service_lift_speed: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  avg_waiting_time: Joi.number().required().messages({
-    'any.required': 'Field name is required',
-    'string.base': 'Field name must be a number'
-  }),
-  // formula: Joi.string().optional(),
-
-  // inputValues: Joi.array().items(
-  //   Joi.object({
-  //     field_id: Joi.number().required().messages({
-  //       'any.required': 'Field ID is required',
-  //       'number.base': 'Field ID must be a number'
-  //     }),
-  //     value: Joi.number()
-  //       .required()
-  //       .min(0)         // 👈 Minimum value
-  //       .max(10000)     // 👈 Maximum value
-  //       .messages({
-  //         'any.required': 'Value is required',
-  //         'number.base': 'Value must be a number',
-  //         'number.min': 'Value must be at least 0',
-  //         'number.max': 'Value must not exceed 10000'
-  //       })
-  //   })
-  // ).optional()
+// Define a common Joi validation pattern for number fields
+const numberRequiredValidation = Joi.number().required().messages({
+  'any.required': requiredMessage,
+  'number.base': typeMessage,
 });
+
+// Array of field names that need the same validation
+const fields = [
+  'project_id', 'module_id', 'building_height', 'carpet_area', 'saleable_area',
+  'balcony_area', 'built_up_area', 'far', 'total_external_wall_area', 'total_basement_area',
+  'circulation_area', 'service_shaft_area', 'window_to_area_ratio', 'drive_away_width_basement',
+  'total_steel_reinforcement', 'total_concrete_consumption', 'air_conditioned_area',
+  'apartment_area', 'club_area', 'total_parking_area', 'total_number_of_cars', 'lobby_corridor_width',
+  'drive_away_width', 'floor_to_floor_height_ground_lobby', 'floor_to_floor_height_basement_b1_b2',
+  'floor_to_floor_height_apt', 'car_length', 'car_width', 'passenger_lift_speed', 'service_lift_speed',
+  'avg_waiting_time',
+];
+
+// Dynamically create the Joi schema based on the field names
+const createKeyRatioFieldSchema = Joi.object(
+  fields.reduce((schema, field) => {
+    schema[field] = numberRequiredValidation;
+    return schema;
+  }, {})
+);
+
+export { createKeyRatioFieldSchema };
